d74672002af28c2f081afa3830acb813ef442978
Poli Mridha <polimridha@gmail.com>
2026-06-14 15:13:57 +0530

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.get("/", (req, res) => {
     res.send("Backend Running");
 })
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`server running ${PORT}`);
-})
+});
