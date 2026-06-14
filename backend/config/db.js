14d56f87cb79e8b8bbdff491a421ce6e2afe9a22
Poli Mridha <polimridha@gmail.com>
2026-06-14 13:10:49 +0530

    Connected MongoDB Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,27 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const connectDB=async()=>{
+const connectDB = async () => {
 
-try{
+    try {
 
-await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(
+            process.env.MONGO_URI
+        );
 
-console.log("MongoDB Connected");
+        console.log(
+            "MongoDB Connected"
+        );
 
-}
+    }
 
-catch(err){
+    catch (err) {
 
-console.log(err);
+        console.log(err);
 
-process.exit(1);
+        process.exit(1);
 
-}
+    }
 
-}
+};
 
-module.exports=connectDB;
+module.exports = connectDB;
